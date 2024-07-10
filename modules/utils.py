8317a6d45d18d425524de690c5e6a2882465e8ab
Katarina Zailac <katarina.zailac@gmail.com>
2024-07-10 13:26:30 +0200

    Reorganize classes in different files

--- modules/utils.py
+++ modules/utils.py
@@ -1,3 +1,22 @@
+import signal
+
+
+class timeout:
+    def __init__(self, seconds=1, error_message="Timeout"):
+        self.seconds = seconds
+        self.error_message = error_message
+
+    def handle_timeout(self, signum, frame):
+        raise TimeoutError(self.error_message)
+
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        signal.alarm(0)
+
+
 class ServiceStatus:
     OK = 0
     WARNING = 1
